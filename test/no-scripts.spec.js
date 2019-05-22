7c2c8805f965b212ba96dffa96d081a84e2cc928
N Oliver <github@pixnbits.org>
2019-05-21 23:05:49 -0700

    feat(postinstall): warn when no hooks were installed various scenarios like no scripts field, no githook scripts, no valid githook scripts

--- test/no-scripts.spec.js
+++ test/no-scripts.spec.js
@@ -7,9 +7,21 @@ var expect = chai.expect;
 chai.use(chaiFiles);
 
 describe('no scripts', function () {
-  after(utils.teardown);
+  afterEach(utils.teardown);
 
   it('should not fail if a package.json does not have a scripts field', function () {
     expect(function () { utils.setup(undefined); }).not.to.throw();
+  });
+
+  it('should warn if a package.json does not have a scripts field', function () {
+    var logLines = utils.setup(undefined);
+    expect(logLines).to.have.length(1);
+    expect(logLines[0]).to.equal('githook-scripts: no "scripts" field in package.json, skipping');
+  });
+
+  it('should warn if a package.json does not have any scripts defined', function () {
+    var logLines = utils.setup({ });
+    expect(logLines).to.have.length(1);
+    expect(logLines[0]).to.equal('githook-scripts: no githook scripts found, add one (ex: "githook:pre-commit") to package.json and run "npm rebuild githook-scripts" to activate');
   });
 });
